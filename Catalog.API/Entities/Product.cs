a911dc113d18c7f79608ba5ff03fe7dbffad7989
DESKTOP-BQMKSQO\Levent <levent.ozturk@mobven.com>
2021-01-27 17:01:36 +0300

    Add project files.

---
+++ Catalog.API/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+
+    }
+}
